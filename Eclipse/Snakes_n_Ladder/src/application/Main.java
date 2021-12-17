1ffdb0eb222a3954c154c241856f0ce245826a3c
Ayush_Singal <ayush20365@iiitd.ac.in>
2021-12-17 23:36:15 +0530

    img added

--- Eclipse/Snakes_n_Ladder/src/application/Main.java
+++ Eclipse/Snakes_n_Ladder/src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,13 +13,14 @@ import java.util.Objects;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage Stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("application.css")).toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Stage.setScene(scene);
+			
+			Stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- Eclipse/Snakes_n_Ladder/src/module-info.java
+++ Eclipse/Snakes_n_Ladder/src/module-info.java
@@ -1,5 +1,7 @@
 module Snakes_n_Ladder {
 	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }

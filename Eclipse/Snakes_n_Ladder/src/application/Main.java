30ed7e123b708c3cd7d35c594607d5bfe95c1306
pranav20395 <88434431+pranav20395@users.noreply.github.com>
2021-12-19 13:42:21 +0530

    icon

--- Eclipse/Snakes_n_Ladder/src/application/Main.java
+++ Eclipse/Snakes_n_Ladder/src/application/Main.java
@@ -3,6 +3,8 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.ImageCursor;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,6 +23,9 @@ public class Main extends Application {
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("application.css")).toExternalForm());
 			Stage.setScene(scene);
+			Stage.setTitle("Snakes & Ladders");
+			Image logo = new Image("game.png");
+			Stage.getIcons().add(logo);
 			
 			Stage.show();
 		} catch(Exception e) {
